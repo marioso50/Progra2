3fdafe81e407c2f0d1de921ff0b5e6305fc59df4
mario <mario@DESKTOP-ENACMEK>
2019-07-19 15:56:24 -0600

    ejercicio 3

---
+++ VariableNombreDireccionTlefono/src/variablenombredirecciontlefono/VariableNombreDireccionTlefono.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variablenombredirecciontlefono;
+
+/**
+ *
+ * @author mario
+ */
+public class VariableNombreDireccionTlefono {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Mario Josue Martinez Olivares";
+    String direccion = "Monjas, - Jalapa";
+    String telefono = "Tel: 31 84 70 03";
+    System.out.println(nombre);
+    System.out.println(direccion);
+    System.out.println(telefono);
+    }
+    
+}
