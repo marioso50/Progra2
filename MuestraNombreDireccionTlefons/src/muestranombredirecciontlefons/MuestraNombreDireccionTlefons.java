459bfbd1ecde94bcb737e79c97b0595c59c2ebff
mario <mario@DESKTOP-ENACMEK>
2019-07-19 12:29:11 -0600

    ejercicio2

---
+++ MuestraNombreDireccionTlefons/src/muestranombredirecciontlefons/MuestraNombreDireccionTlefons.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muestranombredirecciontlefons;
+
+/**
+ *
+ * @author mario
+ */
+public class MuestraNombreDireccionTlefons {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("NOMBRE:Mario Josue Martinez Olivares");
+    System.out.println("DIRECCION:Monjas, Jalapa");
+    System.out.println("TELEFONO:31847003");
+    }
+    
+}
