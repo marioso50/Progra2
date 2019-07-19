cd26564bdc2503c2fae3d94c0afd476ce152c417
mario <mario@DESKTOP-ENACMEK>
2019-07-19 11:20:46 -0600

    tarea1

--- JavaApplication82/src/javaapplication82/JavaApplication82.java
+++ JavaApplication82/src/javaapplication82/JavaApplication82.java
@@ -16,6 +16,8 @@ public class JavaApplication82 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        ehgasdjkgljaopg
     }
     
 }
