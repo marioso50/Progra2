74578c22f98d44a5056a41b14822ba39889c2237
mario <mario@DESKTOP-ENACMEK>
2019-07-19 12:39:08 -0600

    ejercicio3

---
+++ 10PalabrasEnIngles/src/pkg10palabraseningles/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg10palabraseningles;
+
+/**
+ *
+ * @author mario
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("car\tcarro");
+		System.out.println("music\tmusica");
+		System.out.println("dog\tperro");
+		System.out.println("white\tblanco");
+		System.out.println("bottle\tbotella");
+		System.out.println("work\ttrabajo");
+		System.out.println("light\tluz");
+		System.out.println("air\taire"); 
+		System.out.println("shoes\tzapatos");
+	        System.out.println("book\tlibro");
+    }
+    
+}
