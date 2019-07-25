f156093bd682ba0e1a373bec63b38ddd6cb156b9
mario <mario@DESKTOP-ENACMEK>
2019-07-24 20:57:27 -0600

    tarea 1 semana 1

--- progra2/src/p2/semana1/tarea1/Ejercicio6.java
+++ progra2/src/p2/semana1/tarea1/Ejercicio6.java
@@ -10,6 +10,8 @@ package p2.semana1.tarea1;
  * @author mario
  */
 public class Ejercicio6 {
+     public static void main(String[] args)
+     {
      System.out.println("    *");		
 		System.out.println("   ***");
 		System.out.println("  *****");
@@ -21,4 +23,5 @@ public class Ejercicio6 {
     System.out.println("  *****");
     System.out.println(" *******");
     System.out.println("*********");
+}
 }
